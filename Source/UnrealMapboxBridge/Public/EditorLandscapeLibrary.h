ae73d9944d329ea0a8d27edf9c7178c79874b49b
Daniel Elebash <delebash@users.noreply.github.com>
2022-11-28 17:17:33 -0500

    remove old code, add world partition

--- Source/UnrealMapboxBridge/Public/EditorLandscapeLibrary.h
+++ Source/UnrealMapboxBridge/Public/EditorLandscapeLibrary.h
@@ -15,28 +15,12 @@ class UNREALMAPBOXBRIDGE_API UEditorLandscapeLibrary
 	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintCallable, Category = "Editor Scripting | Landscape")
-	static void RunExe(FString PathToExe);
-	
-	UFUNCTION(BlueprintCallable, Category = "Editor Scripting | Landscape")
-	static void CreateLandscapeFeatures(float CoordinateX, float CoordinateY, float OriginNorthing, float OriginEasting, FString HeightMapDirectory,
-													  FString TileHeightmapFileName, int Counter);
-	
-	UFUNCTION(BlueprintCallable, Category = "Editor Scripting | Landscape")
+
+	UFUNCTION(BlueprintCallable, Category = "Dan Editor Scripting | Landscape")
 	static void ImportHeightmap(FString FilePath, FString LandscapeName,
-	                            FString FileName, int LandscapeSize);
-
-	UFUNCTION(BlueprintCallable, Category = "Editor Scripting | Landscape")
-	static void ImportWeightmap(ALandscapeProxy* Landscape, const FString& LayerName,
-	                            const FString& FilePath = TEXT(""));
-
-	UFUNCTION(BlueprintCallable, Category = "FolderPicker")
-	static void OpenFolderDialog(const FString& DialogTitle, const FString& DefaultPath,
-	                             FString& OutFolderName);
+							FString FileName, const int Width, const int Height,const int WorldPartitionGridSize);
 
 private:
 	static bool ReadHeightmapFile(TArray<uint16>& Result, const FString& Filename, int32 ExpectedWidth,
-	                              int32 ExpectedHeight);
-	static bool ReadWeightmapFile(TArray<uint8>& Result, const FString& Filename, FName LayerName, int32 ExpectedWidth,
 	                              int32 ExpectedHeight);
 };
