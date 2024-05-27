a9fe8a47468ba69aa0373cd6798cbe1746166bdc
Daniel Elebash <delebash@users.noreply.github.com>
2024-05-27 07:19:52 -0400

    fixed road generator still experimental and has some problems

---
+++ Source/UnrealMapboxBridge/Public/GeoJsonStruct.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "GeoJsonStruct.generated.h"
+
+USTRUCT(BlueprintType)
+struct FGeoJsonStruct
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	UPROPERTY(EditAnyWhere, BluePrintReadWrite, Category = "GeoJson")
+	FString GeometryType;
+
+	UPROPERTY(EditAnyWhere, BluePrintReadWrite, Category = "GeoJson")
+	FString Type;
+
+	// UPROPERTY(EditAnyWhere, BluePrintReadWrite, Category = "GeoJson")
+	// TArray<FVector> GeometryCoordinates;
+
+public:
+	static FGeoJsonStruct BuildGeoJsonStruct(
+		FString GeometryType,
+		FString Type
+	//	TArray<FVector> GeometryCoordinates
+	);
+};
+
+//
+// UPROPERTY()
+// FJsonObject properties;
+//
+// UPROPERTY()
+// FJsonObject layer;
+//
+// UPROPERTY()
+// FString source;
+//
+// UPROPERTY()
+// FJsonObject state;
